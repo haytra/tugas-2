0a98bc631dd5f416ebac6cb90ca74d9e890bbbad
Prabuae <ulhaq5276@gmail.com>
2025-01-21 09:08:40 +0700

    punyaku

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\buku;
 use App\Models\User;
+use App\Models\ulasan;
+use App\Models\anggota;
+use App\Models\penulis;
+use App\Models\rakbuku;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\peminjaman;
+use App\Models\pustakawan;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +20,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+       
+        $pustakawans = pustakawan::factory(3)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($pustakawans as $pustakawan) {
+            $anggotas = anggota::factory(3)->create(['pustakawan_id' => $pustakawan->id]);
+
+            foreach ($anggotas as $anggota) {
+                $peminjamans = peminjaman::factory(3)->create(['anggota_id' => $anggota->id]);
+
+                foreach ($peminjamans as $peminjaman) {
+                    $ulasans = ulasan::factory(3)->create(['peminjaman_id' => $peminjaman->id]);
+
+                    // foreach ($kecamatans as $kecamatan) {
+                    //     $desas = Desa::factory(3)->create(['kecamatan_id' => $kecamatan->id]);
+
+                        foreach ($ulasans as $ulasan) {
+                            $rakbukus = rakbuku::factory(10)->create(['ulasan_id' => $ulasan->id]);
+
+                            for ($i = 0; $i < count($rakbukus); $i++) {
+                                penulis::factory()->create();
+                                buku::factory()->create();
+                                User::factory()->create();
+                                // rakable::factory()->create();
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+        }
     }
-}
+    
